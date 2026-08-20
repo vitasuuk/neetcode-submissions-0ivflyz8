60002c65e388393306f740f8fa91dee21b6dac40
Vitalii Khlivetskyy <vitalikkhl@gmail.com>
2026-08-19 18:05:59 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0;
+        int sell = 1;
+
+        int maxProfit = 0;
+
+        while (sell < prices.length) {
+            if (prices[sell] > prices[buy]) {
+                int profit = prices[sell] - prices[buy];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buy = sell;
+            }
+
+            sell++;
+        }
+
+
+        return maxProfit;
+    }
+}
